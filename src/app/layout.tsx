5498e2087e236279f35be15f8c3da1be3b47edbc
k1ngzadapkrl <kngdudiscord@gmail.com>
2026-01-13 13:23:31 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
 
-const NUBANK_LOGO = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Nubank_logo_2021.svg/1200px-Nubank_logo_2021.svg.png";
+const NUBANK_LOGO = "https://logodownload.org/wp-content/uploads/2019/08/nubank-logo-2-1.png";
 
 export const metadata: Metadata = {
   // Deixamos o título como um espaço ou algo genérico para o navegador
