9a561a9446f19de753b7684c67d1f3c406a909bd
sdaid12891203jdhsuadh <k1ngdodiscordtmj@gmail.com>
2026-01-16 12:32:22 -0300

    Create OSSelect.tsx

---
+++ src/app/components/OSSelect.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+// Criamos a função OSSelect e passamos o setSelectedOs e setView como "ferramentas"
+export default function OSSelect({ setSelectedOs, setView }: any) {
+  return (
+    <div className="fixed inset-0 bg-[#070707] flex flex-col justify-center p-8 z-25">
+      <h2 className="text-white text-center mb-8 font-bold text-xl uppercase tracking-widest">
+        Selecione o Sistema
+      </h2>
+      <div className="space-y-4">
+        <button 
+          onClick={() => { setSelectedOs('android'); setView('version_select'); }} 
+          className="w-full bg-[#111] p-6 rounded-2xl flex justify-between text-white font-bold border border-zinc-900 active:bg-[#1a1a1a]"
+        >
+          <span>ANDROID</span><span>→</span>
+        </button>
+        <button 
+          onClick={() => { setSelectedOs('ios'); setView('version_select'); }} 
+          className="w-full bg-[#111] p-6 rounded-2xl flex justify-between text-white font-bold border border-zinc-900 active:bg-[#1a1a1a]"
+        >
+          <span>IOS</span><span>→</span>
+        </button>
+      </div>
+    </div>
+  )
+}
